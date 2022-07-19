f76c719181c5d4b78edef9d87e64f185f16b70f6
MatthewPress <press.matt14@gmail.com>
2022-07-19 03:24:29 -0600

    refactor: rename variables and functions

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var dishSelectorButton = document.querySelector("#dish-selector");
+var dishSelection = document.getElementsByName("dish-selection");
+var dishGeneratorButton = document.querySelector("#dish-selector");
 var potPage = document.querySelector(".pot-page");
 var dishPage = document.querySelector(".dish-page");
+var dishOutput = document.querySelector("#dish");
 var clearDishButton = document.querySelector("#clear-dish");
-var dish = document.querySelector("#dish");
-var dishSelection = document.getElementsByName("dish-selection");
 
-var menus = {
+
+
+var menu = {
   sides: [
     "Miso Glazed Carrots",
     "Coleslaw",
@@ -29,26 +31,26 @@ var menus = {
   ]
 };
 
-dishSelectorButton.addEventListener("click", getRandomDish);
-clearDishButton.addEventListener("click", displayDishPage);
+dishGeneratorButton.addEventListener("click", displayRandomDish);
+clearDishButton.addEventListener("click", toggleDishPage);
 
 function getRandomIndex(dishArray) {
   return Math.floor(Math.random() * dishArray.length);
 }
 
-function getRandomDish() {
-  displayDishPage();
+function displayRandomDish() {
+  toggleDishPage();
 
   for (var i = 0; i < dishSelection.length; i++) {
     if (dishSelection[i].checked) {
-      var dishOptions = menus[dishSelection[i].value];
-      dish.innerText = `${dishOptions[getRandomIndex(dishOptions)]}!`;
+      var dishOptions = menu[dishSelection[i].value];
+      dishOutput.innerText = `${dishOptions[getRandomIndex(dishOptions)]}!`;
     }
   }
 }
 
-function displayDishPage() {
-  dish.innerText = "";
+function toggleDishPage() {
+  dishOutput.innerText = "";
   potPage.classList.toggle("hidden");
   dishPage.classList.toggle("hidden");
 }
