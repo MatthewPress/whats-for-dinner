38b967096b0a2622c6ab8afb284004059dd77cb6
MatthewPress <press.matt14@gmail.com>
2022-07-22 01:52:13 -0600

    refactor: turing style guide and dry

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-var dishSelection = document.getElementsByName("dish-selection");
-var dishGeneratorButton = document.querySelector(".dish-generator");
-var potPage = document.querySelector(".pot-page");
-var dishPage = document.querySelector(".dish-page");
-var dishOutput = document.querySelector(".dish");
-var clearDishButton = document.querySelector(".clear-dish");
-var form = document.querySelector("form");
+var dishSelection = document.getElementsByName('dish-selection');
+var dishGeneratorButton = document.querySelector('.dish-generator');
+var potPage = document.querySelector('.pot-page');
+var dishPage = document.querySelector('.dish-page');
+var dishOutput = document.querySelector('.dish-output');
+var clearDishButton = document.querySelector('.clear-dish');
+var form = document.querySelector('form');
 
 var menu = {
   sides: [
-    "Miso Glazed Carrots",
-    "Coleslaw",
-    "Garden Salad",
-    "Crispy Potatoes",
-    "Sweet Potato Tots"
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Sweet Potato Tots'
   ],
   mains: [
-    "Spaghetti and Meatballs",
-    "Pineapple Chicken",
-    "Shakshuka",
-    "Thai Yellow Curry",
-    "Bibimbap"
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap'
   ],
   desserts: [
-    "Apple Pie",
-    "Lemon Meringue Pie",
-    "Black Forest Cake",
-    "Banana Bread",
-    "Peach Cobbler"
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler'
   ]
 };
 
-form.addEventListener("change", displayDishGenerator);
-dishGeneratorButton.addEventListener("click", displayRandomDish);
-clearDishButton.addEventListener("click", displayPotPage);
+form.addEventListener('change', displayDishGenerator);
+dishGeneratorButton.addEventListener('click', displayRandomDish);
+clearDishButton.addEventListener('click', clearPages);
 
-function getRandomIndex(dishArray) {
-  return Math.floor(Math.random() * dishArray.length);
+function displayDishGenerator() {
+  dishGeneratorButton.classList.remove('hidden');
 }
 
 function displayRandomDish() {
@@ -49,25 +49,25 @@ function displayRandomDish() {
   }
 }
 
-function displayDishPage() {
-  dishOutput.innerText = "";
-  potPage.classList.add("hidden");
-  dishPage.classList.remove("hidden");
+function getRandomIndex(dishArray) {
+  return Math.floor(Math.random() * dishArray.length);
 }
 
-function displayPotPage() {
-  dishOutput.innerText = "";
-  potPage.classList.remove("hidden");
-  dishPage.classList.add("hidden");
-  dishGeneratorButton.classList.add("hidden");
+function displayDishPage() {
+  dishOutput.innerText = '';
+  potPage.classList.add('hidden');
+  dishPage.classList.remove('hidden');
+}
+
+function clearPages() {
+  dishOutput.innerText = '';
+  potPage.classList.remove('hidden');
+  dishPage.classList.add('hidden');
+  dishGeneratorButton.classList.add('hidden');
 
   for (var i = 0; i < dishSelection.length; i++) {
     if (dishSelection[i].checked) {
       dishSelection[i].checked = false;
     }
   }
-}
-
-function displayDishGenerator() {
-  dishGeneratorButton.classList.remove("hidden");
 }
